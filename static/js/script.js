0549df55ece475953293270d3f81f6d4691269fa
PR7175Z <aadimpraz@gmail.com>
2024-09-21 15:05:29 +0545

    Code to show data

--- static/js/script.js
+++ static/js/script.js
@@ -13,45 +13,43 @@ async function processResponse() {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        console.log(data)
         return {data};   
     }
 
     groupedData = await get_response();
 
     for(let i=0; i<Object.keys(groupedData.data).length; i++){
-        // console.log(`group ${i} : ${groupedData.data[i]}`)
-
         let parentDiv = document.createElement('div');
         parentDiv.classList.add('col-md-3');
 
-        // for(let k=0; k<Object.keys(groupedData.data[i]).length; k++){
-        //     console.log(`${i}, ${k}`)
-        // }
         for (let key in groupedData.data[i]) {
-            console.log(`${i} , ${key}`);
             let div = document.createElement('div');
             div.classList.add('groupitems')
             let name = document.createElement('p');
-            let skill = document.createElement('p');
             let email = document.createElement('p');
+            let skill = document.createElement('p');
             name.innerHTML = groupedData.data[i][key]['Name'];
-            skill.innerHTML = groupedData.data[i][key]['skills'];
             email.innerHTML = groupedData.data[i][key]['Email'];
+
+            coding = groupedData.data[i][key]['Coding'];
+            Leadership = groupedData.data[i][key]['Leadership'];
+            communication = groupedData.data[i][key]['Communication Skill'];
+            presentation = groupedData.data[i][key]['Presentation designing'];
+
+            skills = {'Coding': coding, 'Leadership': Leadership, 'Communication' : communication, 'Presentation': presentation}
+            const topTwoSpecialities = Object.entries(skills).sort(([, a], [, b]) => b - a).slice(0, 2).map(([key]) => key); 
             div.appendChild(name);
-            div.appendChild(skill);
             div.appendChild(email);
-            // div.innerHTML = groupedData.data[i][key]['Email'];
+
+            topTwoSpecialities.forEach((e)=>{
+                const specialityElement = document.createElement('span');
+                specialityElement.textContent = e;
+                skill.appendChild(specialityElement);
+            })
             parentDiv.appendChild(div);
-            console.log(`Key: ${key}, Value:`, groupedData.data[i][key]);
         }
         document.getElementById('itemdisplay').appendChild(parentDiv);
-        // console.log(groupedData.data[i]);
     }
-
-    // groupedData.data.forEach((element) => {
-    //     console.log(element);
-    // });
 }
 processResponse();
 
