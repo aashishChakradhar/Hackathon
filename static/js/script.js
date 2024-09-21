c212e497f5672626f51c3abcc744baea3bc78fce
PR7175Z <aadimpraz@gmail.com>
2024-09-21 15:15:15 +0545

    Appended skills

--- static/js/script.js
+++ static/js/script.js
@@ -13,24 +13,26 @@ async function processResponse() {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        return {data};   
+        return {data}; 
     }
 
     groupedData = await get_response();
 
     for(let i=0; i<Object.keys(groupedData.data).length; i++){
+
         let parentDiv = document.createElement('div');
         parentDiv.classList.add('col-md-3');
 
         for (let key in groupedData.data[i]) {
-            let div = document.createElement('div');
-            div.classList.add('groupitems')
+            const div = document.createElement('div');
             let name = document.createElement('p');
             let email = document.createElement('p');
             let skill = document.createElement('p');
+
+            div.classList.add('groupitems')
+
             name.innerHTML = groupedData.data[i][key]['Name'];
             email.innerHTML = groupedData.data[i][key]['Email'];
-
             coding = groupedData.data[i][key]['Coding'];
             Leadership = groupedData.data[i][key]['Leadership'];
             communication = groupedData.data[i][key]['Communication Skill'];
@@ -38,14 +40,17 @@ async function processResponse() {
 
             skills = {'Coding': coding, 'Leadership': Leadership, 'Communication' : communication, 'Presentation': presentation}
             const topTwoSpecialities = Object.entries(skills).sort(([, a], [, b]) => b - a).slice(0, 2).map(([key]) => key); 
-            div.appendChild(name);
-            div.appendChild(email);
 
             topTwoSpecialities.forEach((e)=>{
                 const specialityElement = document.createElement('span');
                 specialityElement.textContent = e;
                 skill.appendChild(specialityElement);
             })
+
+            div.appendChild(name);
+            div.appendChild(email);
+            div.appendChild(skill);
+
             parentDiv.appendChild(div);
         }
         document.getElementById('itemdisplay').appendChild(parentDiv);
